679545e9ba04117f6085f14104e23d29ae399e72
Erick Renaud <renaud_junior@outlook.com>
2024-08-20 23:55:54 -0400

    Added api/category get route
    This route finds all categories and includes there product and returns the entire array

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,26 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const categories = await Category.findAll({
+      include: Product,
+    });
+
+    if(!categories){
+      res.status(404).json('No categories found');
+      return;
+    }
+
+    res.status(200).json(categories);
+    return;
+  } catch(err){
+    res.status(500).json(err);
+  }
+
+  console.log(categories)
 });
 
 router.get('/:id', (req, res) => {
