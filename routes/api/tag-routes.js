b221abdf076b441cf9bbdc5b29ccd8b45c9dc0ed
Erick Renaud <renaud_junior@outlook.com>
2024-08-22 21:00:11 -0400

    Added api/tag put route
    This route updates a given tag after ensuring the params and body are validated and returns the updated values

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -60,12 +60,57 @@ router.get('/:id', async(req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async(req, res) => {
+  if(!req.body.name){
+    res.status(400).json('Invalid request');
+    return;
+  }
+
+  try{
+    const newTag = await Tag.create({tag_name: req.body.name});
+
+    if(!newTag){
+      res.status(500).json('Tag not created');
+      return;
+    }
+
+    res.status(200).json(newTag);
+    return;
+  }catch(err){
+    res.status(500).json('Internal server error');
+    return;
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async(req, res) => {
+  if(isNaN(req.params.id) || !req.body.name){
+    res.status(400).json('Invalid request');
+  };
+
+  try {
+    const tag = await Tag.update(
+      {
+        tag_name: req.body.name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+        returning: true,
+      },
+    );
+
+    if(!tag){
+      res.status(500).json('Tag not created');
+      return;
+    }
+
+    res.status(201).json(tag);
+    return;
+  }catch (err) {
+    res.status(500).json('Internal server error');
+    return;
+  }
 });
 
 router.delete('/:id', (req, res) => {
